0f4dbc97eeae112be1e624faa85ebcb768ca8a60
Winstone <Winstone1108@naver.com>
2022-10-04 11:41:48 +0900

    Step_07

--- Step_07/src/Main.java
+++ Step_07/src/Main.java
@@ -131,6 +131,22 @@ public class Main {
                     System.out.println("for : 홍길동");
                 }
             }
+
+            //====================================================================
+
+            for(int i = 2 ; i < 10 ; ++i)
+            {
+                System.out.println("[" + i + "] 단");
+
+                for(int j = 1 ; j < 10 ; ++j)
+                {
+                    if (j%2==1)
+                        continue;
+
+                    System.out.println( i + " * " + j + " = " + i * j);
+                }
+                System.out.println("\n");
+            }
         }
     }
 }
