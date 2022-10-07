1e8a277912ae566b79a200516e90a44473d00b8d
Winstone <Winstone1108@naver.com>
2022-10-07 13:29:53 +0900

    getter_setter

---
+++ Step_11/src/mega/MemberMain.java
@@ -0,0 +1,66 @@
+package mega;
+
+public class MemberMain {
+
+    //private Member m;
+    //private Member m2;
+
+    public static void main(String[] args)
+    {
+        // =============================================
+        // [메모리구조 확인!!!]
+        // =============================================
+        Member m = new Member();
+        Member m2 = new Member();
+
+        if(m == m2)
+            System.out.println("같다");
+        else
+            System.out.println("다르다");
+
+        // =============================================
+        // =============================================
+
+        String str1 = "홍길동";
+        String str2 = "홍길동";
+
+        if(str1 == str2)
+            System.out.println("같다");
+        else
+            System.out.println("다르다");
+
+        // =============================================
+        //  [객체를 배열로 사용]
+        // =============================================
+        {
+            //Member mArray1;
+            //Member mArray2;
+            //Member mArray3;
+            //Member mArray4;
+            //Member mArray5;
+
+            Member[] mArray = new Member[5];
+
+            mArray[0] = new Member();
+            mArray[0].setId("Key1");
+
+            mArray[1] = new Member();
+            mArray[1].setId("Key2");
+
+            mArray[2] = new Member();
+            mArray[2].setId("Key3");
+
+            mArray[3] = new Member();
+            mArray[3].setId("Key4");
+
+            mArray[4] = new Member();
+            mArray[4].setId("Key5");
+
+            for(Member element : mArray)
+            {
+                String str = element.getId();
+                System.out.println(str);
+            }
+        }
+    }
+}
